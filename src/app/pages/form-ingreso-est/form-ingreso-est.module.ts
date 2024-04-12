18da4a4492e8b6d70f370e0d2daba8d922816677
Ragnaars <orl.tobar@duocuc.cl>
2024-04-12 07:18:06 -0400

    Formulario de ingreso basico, falta tratar validaciones entre otras cosas

--- src/app/pages/form-ingreso-est/form-ingreso-est.module.ts
+++ src/app/pages/form-ingreso-est/form-ingreso-est.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-
+import { ReactiveFormsModule, FormControl, FormGroup } from "@angular/forms"
 import { IonicModule } from '@ionic/angular';
 
 import { FormIngresoEstPageRoutingModule } from './form-ingreso-est-routing.module';
@@ -13,8 +13,9 @@ import { FormIngresoEstPage } from './form-ingreso-est.page';
     CommonModule,
     FormsModule,
     IonicModule,
+    ReactiveFormsModule,
     FormIngresoEstPageRoutingModule
   ],
   declarations: [FormIngresoEstPage]
 })
-export class FormIngresoEstPageModule {}
+export class FormIngresoEstPageModule { }
--- src/app/pages/form-ingreso-est/form-ingreso-est.page.ts
+++ src/app/pages/form-ingreso-est/form-ingreso-est.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ReactiveFormsModule, FormGroup, FormBuilder, FormControl, AbstractControl, ValidatorFn, Validators } from "@angular/forms"
 
 @Component({
   selector: 'app-form-ingreso-est',
@@ -7,9 +8,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormIngresoEstPage implements OnInit {
 
-  constructor() { }
+  formIngresoEst: FormGroup;
+
+  constructor() {
+
+    this.formIngresoEst = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      patente: new FormControl('', [Validators.required, this.patenteChilenaValidator()]),
+      tipoEst: new FormControl('regular', [Validators.required])
+    })
+  }
 
   ngOnInit() {
+  }
+
+  registrarEntradaEst() {
+    console.log(this.formIngresoEst)
+  }
+
+  patenteChilenaValidator(): ValidatorFn {
+    return (control: AbstractControl): { [key: string]: any } | null => {
+      const patente = control.value;
+      console.log("patente", patente);
+      const patron = /^[A-Za-z]{2}\d{2}(\d{1}|[A-Za-z]{1})\d{2}$/;
+
+      if (patron.test(patente)) {
+        return null; // Patente válida, devuelve null
+      } else {
+        return { 'patenteInvalida': { value: patente } }; // Patente inválida, devuelve un objeto con la clave 'patenteInvalida'
+      }
+    };
   }
 
 }
